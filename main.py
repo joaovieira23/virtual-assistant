83be28cc882ece0fab9eb213c49d2f6a3207d396
João Victor <joaovictorvieira.23@hotmail.com>
2020-12-22 22:04:45 -0300

    feat: play music by voice

--- main.py
+++ main.py
@@ -1,20 +1,39 @@
 import speech_recognition as sr
 import pyttsx3
+import pywhatkit
 
 listener = sr.Recognizer()
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)
-engine.say('Eu sou a Alexa')
-engine.say('Em que posso te ajudar?')
-engine.runAndWait()
-try:
-    with sr.Microphone() as source:
-        print('Ouvindo...')
-        voice = listener.listen(source)
-        command = listener.recognize_google(voice)
-        command = command.lower()
-        if 'alexa' in command:
-            print(command)
-except:
-    print("Deu ruim")
+
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
+def take_command():
+    try:
+        with sr.Microphone() as source:
+            print('Ouvindo...')
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            if 'alexa' in command:
+                command = command.replace('alexa', '')
+    except:
+        pass
+    return command
+
+
+def run_alexa():
+    command = take_command()
+    print(command)
+    if 'play' in command:
+        song = command.replace('play', '')
+        talk('playing' + song)
+        pywhatkit.playonyt(song)
+
+
+run_alexa()
